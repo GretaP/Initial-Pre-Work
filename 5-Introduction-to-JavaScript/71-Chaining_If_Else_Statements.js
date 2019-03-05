0a2e8bdb29a1e93786def5f7da7198ee7384e0a6
Greta <gretaparks@gmail.com>
2019-03-04 17:41:19 -0700

    Add JS 57-72

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,16 @@
+// reminds me of Goldilocks and the three bears
+
+function testSize(num) {
+  // Only change code below this line
+  var changeMe;
+  if (num < 5) changeMe = "Tiny";
+  else if(num < 10)changeMe = "Small";
+  else if(num < 15) changeMe = "Medium";
+  else if(num < 20) changeMe = "Large";
+  else changeMe = "Huge";
+  return changeMe;
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
