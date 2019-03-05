4f7dad42f77ae6af9e86f594c2772a5e1754190f
Greta <gretaparks@gmail.com>
2019-03-04 17:11:32 -0700

    Add JS 46-56

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,11 @@
+// functionWithArgs
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(x) {
+  return x*5;
+}
+console.log(timesFive(10));
---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+//functions and scope again
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum+5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
