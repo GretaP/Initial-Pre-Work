542a806c2353978a6b9f1db3794bbb7b22083f97
Greta <gretaparks@gmail.com>
2019-03-04 18:15:37 -0700

    Add JS 73-78

---
+++ 5-Introduction-to-JavaScript/76-Replacing_If_Else_Chains_with_Switch.js
@@ -0,0 +1,19 @@
+// switches ... more of them.
+
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){
+    case "bob":answer = "Marley"; break;
+    case 42: answer = "The Answer"; break;
+    case 1: answer = "There is no #1";break;
+    case 99: answer = "Missed me by this much!";break;
+    case 7:  answer = "Ate Nine"; break;
+    default: answer=""; break;
+  }
+//nly change code above this line
+  return answer;
+}
+
+// Change this value to test
+chainToSwitch(7);
---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+// booleans again
+
+function isLess(a, b) {
+  // Fix this code
+  return a<b;
+}
+// Change these values to test
+isLess(10, 15);
