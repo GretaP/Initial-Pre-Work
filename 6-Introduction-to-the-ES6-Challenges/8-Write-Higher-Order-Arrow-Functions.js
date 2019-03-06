58e9d08692e14cda88325bc348139984b93f252e
Greta <gretaparks@gmail.com>
2019-03-06 15:20:05 -0700

    Add comment to ES6 8

--- 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -1,4 +1,4 @@
-//
+// Learned: I want to learn how to use map more often.  I'm not sure if I've used both filter and map at the same time.  I feel like eventually it would be nice to refactor this, because I think there could be a better solution.
 
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
