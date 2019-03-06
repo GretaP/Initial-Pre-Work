4d9be75e251feaea9db5876f5d1e2f264a5203e6
Greta <gretaparks@gmail.com>
2019-03-06 16:21:35 -0700

    Add JS 99 and ES6 9

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+// Recap: I haven't used do...while for a very long time, so it is a good reminder it is there to be used
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+}while (i < 5);
